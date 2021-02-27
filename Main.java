3b40ed914a490ef8b1d10a0318a978e6e4ce0b3a
محمد ابوالقاسم سيد محمد <mhamed_kassem@yahoo.com>
2021-02-27 02:24:05 +0200

    Revert "Calculator console app"

--- Main.java
+++ Main.java
@@ -1,56 +1,21 @@
 package com.example.java;
 
-import utilities.mathHelper;
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Main calc=new Main();
-        try {
-            calc.calculate();
-        }catch (NumberFormatException e){
-            System.out.println("invalid data type");
-        }
+        byte b=1;
+        long l=1L;
+        short sh=1;
+        int i=1;
+        float f=1F;
+        double d=1d;
+        System.out.println("byte: "+b);
+        System.out.println("long: "+l);
+        System.out.println("short: "+sh);
+        System.out.println("integer: "+i);
+        System.out.println("float: "+f);
+        System.out.println("double: "+d);
 
     }
-
-    protected void calculate() {
-        System.out.println("NOTE That press 'ENTER' KEY after any input data");
-        inputHelper input = new inputHelper();
-        String number1 = input.getInput("enter number1");
-        String op = input.getInput("enter the operation( + , - , / , * )");
-        String number2 = input.getInput("enter number2");
-
-
-        double result = 0;
-        if (op.equalsIgnoreCase("+")) {
-            result = mathHelper.add(number1, number2);
-        } else if (op.equalsIgnoreCase("-")) {
-            result = mathHelper.subtract(number1, number2);
-        } else if (op.equalsIgnoreCase("*")) {
-            result = mathHelper.multiplication(number1, number2);
-        } else if (op.equalsIgnoreCase("/")) {
-            result = mathHelper.dividing(number1, number2);
-        } else {
-            System.out.println("the symbol you Enter don`t match any operation");
-        }
-
-        System.out.println("the result is: " + result);
-
-    }
-// the method getInput is enough but the inner class just for practice
-        public static class inputHelper {
-            private String getInput(String prompt) {
-
-                System.out.println(prompt);
-                Scanner scanner = new Scanner(System.in);
-                return scanner.next();
-            }
-        }
-    }
-
-
-
+}
